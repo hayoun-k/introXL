3613f675316faa490280198527a4e00cc491ffc0
Hayoun Kim <hayoun.k.1102@gmail.com>
2025-02-27 16:04:04 -0800

    index main animation

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,10 +1,12 @@
 import gsap from "gsap";
 
 const menu = document.getElementById("mobileMenu");
+const navBar = document.querySelector(".navbar");
 const navList = document.querySelector(".nav-list");
 
 if (window.innerWidth < 1024) {
-  gsap.set(navList, {height: 0});
+  gsap.set(navBar, {height: 0});
+  // gsap.set(navList, {height: 0});
 }
 
 
@@ -13,9 +15,11 @@ menu.addEventListener("click", () => {
   navList.classList.toggle("active");
 
   if (menu.classList.contains("active")) {
-    gsap.to(navList, {height: "auto", duration: 0.3, ease: "power1.inOut"});
+    gsap.to(navBar, {height: "auto", duration: 0.3, ease: "power1.inOut"});
+    // gsap.to(navList, {height: "auto", duration: 0.3, ease: "power1.inOut"});
   } else {
-    gsap.to(navList, { height: 0, duration: 0.3, ease: "power1.inOut"});
+    // gsap.to(navList, { height: 0, duration: 0.3, ease: "power1.inOut"});
+    gsap.to(navBar, { height: 0, duration: 0.3, ease: "power1.inOut"});
   }
 
 });
